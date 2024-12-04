6b50a3c6853f84e2bbc68ce88dcf5a6959172e09
Gabriel Alfonzo <bagaao@live.com>
2024-12-04 01:17:06 -0400

    Up to day 2 part 2, need to figure out this one more

--- main.go
+++ main.go
@@ -1,41 +1,186 @@
 package main
 
 import (
+	"bufio"
 	"log"
+	"os"
 	"sort"
 	"strconv"
+	"strings"
 )
 
-func Day1(leftArray []int, rightArray []int) int {
-	log.Print("Starting Day 1 pt1")
+func LoadArrayD1() ([]int, []int) {
+	var leftArray []int
+	var rightArray []int
+	file, err := os.Open("./Input/d1.txt")
+	if err != nil {
+		log.Panicf("Error reading file: %v", err)
+	}
+	defer file.Close()
+	sc := bufio.NewScanner(file)
+	for sc.Scan() {
+		line := sc.Text()
+		fields := strings.Fields(line)
+		lv, err := strconv.Atoi(fields[0])
+		if err != nil {
+			log.Panicf("Error converting left side of array to int: %v", err)
+		}
+		leftArray = append(leftArray, lv)
+		rv, err := strconv.Atoi(fields[1])
+		if err != nil {
+			log.Panicf("Error converting right side of array to int: %v", err)
+		}
+		rightArray = append(rightArray, rv)
 
-	//sort.Ints(rightArray)
-	//sort.Ints(leftArray)
+	}
+	return leftArray, rightArray
+}
+func Day1p1(leftArray []int, rightArray []int) int {
+
+	sort.Ints(rightArray)
+	sort.Ints(leftArray)
 	var distance = 0
 	for index := range leftArray {
-		leftval := intsplit(leftArray[index])
-		rightval := intsplit(rightArray[index])
-		sort.Ints(leftval)
-		sort.Ints(rightval)
-		for i := 0; i < len(leftval); i++ {
-			if leftval[i] > rightval[i] {
-				distance += leftval[i] - rightval[i]
-			} else if leftval[i] < rightval[i] {
-				distance += rightval[i] - leftval[i]
+		if leftArray[index] > rightArray[index] {
+			distance += leftArray[index] - rightArray[index]
+		} else if leftArray[index] < rightArray[index] {
+			distance += rightArray[index] - leftArray[index]
+		}
+	}
+	return distance
+}
+func Day1p2(leftArray []int, rightArray []int) int {
+	sort.Ints(leftArray)
+	sort.Ints(rightArray)
+	var similarity = 0
+	for index := range leftArray {
+		count := 0
+		for _, value := range rightArray {
+			if leftArray[index] == value {
+				count++
+			}
+		}
+		sim := leftArray[index] * count
+		similarity += sim
+	}
+	return similarity
+}
+
+func LoadArrayD2() [][]int {
+	var nestedarray [][]int
+	file, err := os.Open("./Input/d2.txt")
+	if err != nil {
+		log.Panicf("Error reading file: %v", err)
+	}
+	defer file.Close()
+	sc := bufio.NewScanner(file)
+	for sc.Scan() {
+		var arrofval []int
+		line := sc.Text()
+		fields := strings.Fields(line)
+		for _, val := range fields {
+			valint, err := strconv.Atoi(val)
+			if err != nil {
+				log.Panicf("Error converting value to int: %v", err)
+			}
+			arrofval = append(arrofval, valint)
+		}
+		nestedarray = append(nestedarray, arrofval)
+	}
+	return nestedarray
+}
+
+func withinrangeminus(i int, x int) bool {
+	switch {
+	case i-1 == x:
+		return true
+	case i-2 == x:
+		return true
+	case i-3 == x:
+		return true
+	default:
+		return false
+
+	}
+}
+func withinrangemas(i int, x int) bool {
+	switch {
+	case i+1 == x:
+		return true
+	case i+2 == x:
+		return true
+	case i+3 == x:
+		return true
+	default:
+		return false
+	}
+}
+
+func Day2p1(list [][]int) int {
+	var isSafe = 0
+	for _, value := range list {
+		if withinrangemas(value[0], value[1]) {
+			for i, v := range value {
+				if i >= len(value)-1 {
+					isSafe++
+					break
+				}
+				if !withinrangemas(v, value[i+1]) {
+					break
+				}
+			}
+		}
+		if withinrangeminus(value[0], value[1]) {
+			for i, v := range value {
+				if i >= len(value)-1 {
+					isSafe++
+					break
+				}
+				if !withinrangeminus(v, value[i+1]) {
+					break
+				}
 			}
 		}
 
 	}
-	log.Printf("Total for day 1 is: %v", distance)
-	return distance
+	return isSafe
+
 }
 
-func intsplit(in int) []int {
-	nt := strconv.Itoa(in)
-	result := make([]int, len(nt))
-	for i, char := range nt {
-		number, _ := strconv.Atoi(string(char))
-		result[i] = number
+func Day2p2(list [][]int) int {
+	var isSafe = 0
+	for _, value := range list {
+		tolerance := 0
+		if withinrangemas(value[0], value[1]) {
+			for i, v := range value {
+				if i >= len(value)-1 {
+					isSafe++
+					break
+				}
+				if !withinrangemas(v, value[i+1]) {
+					tolerance++
+				}
+				if tolerance >= 1 {
+					break
+				}
+			}
+		}
+		if withinrangeminus(value[0], value[1]) {
+			for i, v := range value {
+				if i >= len(value)-1 {
+					isSafe++
+					break
+				}
+				if !withinrangeminus(v, value[i+1]) {
+					tolerance++
+				}
+				if tolerance >= 1 {
+					break
+				}
+			}
+		}
+
 	}
-	return result
+	return isSafe
+
 }
--- main_test.go
+++ main_test.go
@@ -9,7 +9,49 @@ func TestDay1(t *testing.T) {
 	var testRightArray = []int{4, 3, 5, 3, 9, 3}
 
 	t.Run("Day 1 Part 1 test values", func(t *testing.T) {
-		dis := Day1(testLeftArray, testRightArray)
+		dis := Day1p1(testLeftArray, testRightArray)
+		t.Log("Test run value", dis)
+	})
+	t.Run("Day 1 Part 1 values", func(t *testing.T) {
+		dis := Day1p1(LoadArrayD1())
+		t.Log("Test run value", dis)
+	})
+	t.Run("Day 1 Part 2 test values", func(t *testing.T) {
+		dis := Day1p2(testLeftArray, testRightArray)
+		t.Log("Test run value", dis)
+	})
+	t.Run("Day 1 Part 2 values", func(t *testing.T) {
+		dis := Day1p2(LoadArrayD1())
+		t.Log("Test run value", dis)
+	})
+
+}
+
+func TestDay2(t *testing.T) {
+
+	var testnestedarray = [][]int{
+		{7, 6, 4, 2, 1},
+		{1, 2, 7, 8, 9},
+		{9, 7, 6, 2, 1},
+		{1, 3, 2, 4, 5},
+		{8, 6, 4, 4, 1},
+		{1, 3, 6, 7, 9},
+	}
+	var nestedarray = LoadArrayD2()
+	t.Run("Day 2 Part 1 test values", func(t *testing.T) {
+		dis := Day2p1(testnestedarray)
+		t.Log("Test run value", dis)
+	})
+	t.Run("Day 2 Part 1 values", func(t *testing.T) {
+		dis := Day2p1(nestedarray)
+		t.Log("Test run value", dis)
+	})
+	t.Run("Day 2 Part 2 test values", func(t *testing.T) {
+		dis := Day2p2(testnestedarray)
+		t.Log("Test run value", dis)
+	})
+	t.Run("Day 2 Part 2 test values", func(t *testing.T) {
+		dis := Day2p2(nestedarray)
 		t.Log("Test run value", dis)
 	})
 
